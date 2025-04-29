1354063c35586afb2bebe8acf62731f23160b878
Ivan Pinheiro <Ivanppf1@gmail.com>
2025-04-29 19:34:57 -0300

    First commit

---
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ifpbpj2.SIMULENEM_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulenemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulenemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpbpj2.SIMULENEM_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulenemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
