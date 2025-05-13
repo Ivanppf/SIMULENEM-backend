094ddc91ab3300ad9bdab06fda9c227d342ece1c
robsoncaliban <robsoncaliban@gmail.com>
2025-05-13 16:20:37 -0300

    test: refatorado testes para o método save da classe QuestionServiceImpl, incluindo validação de categorias

--- src/test/java/com/ifpbpj2/SIMULENEM_backend/business/services/QuestionServiceImplTest.java
+++ src/test/java/com/ifpbpj2/SIMULENEM_backend/business/services/QuestionServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ifpbpj2.SIMULENEM_backend.business.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
@@ -9,7 +10,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -22,12 +22,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import com.ifpbpj2.SIMULENEM_backend.model.entities.Alternative;
-import com.ifpbpj2.SIMULENEM_backend.model.entities.Illustration;
+import com.ifpbpj2.SIMULENEM_backend.model.entities.Category;
 import com.ifpbpj2.SIMULENEM_backend.model.entities.Question;
 import com.ifpbpj2.SIMULENEM_backend.model.enums.Difficulty;
+import com.ifpbpj2.SIMULENEM_backend.model.enums.Origin;
 import com.ifpbpj2.SIMULENEM_backend.model.enums.QuestionType;
 import com.ifpbpj2.SIMULENEM_backend.model.repositories.QuestionRepository;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.AlternativesRequestDTO;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.IllustrationRequestDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.QuestionRequestDTO;
 
 @SpringBootTest
@@ -35,6 +37,9 @@ class QuestionServiceImplTest {
 
     @MockitoBean
     QuestionRepository questionRepository;
+
+    @MockitoBean
+    CategoryService categoryService;
 
     @Autowired
     QuestionService questionService;
@@ -48,43 +53,54 @@ class QuestionServiceImplTest {
     }
 
     static void initializeQuestions(){
-        Set<Alternative> alternatives01 = new HashSet<>();
-        Alternative alternative01a = new Alternative('a', "Paris", new Illustration( "Correct answer", "image2.test"));
-        Alternative alternative01b = new Alternative('b', "London", new Illustration("Incorrect answer", "image3.test"));
-        Alternative alternative01c = new Alternative('c', "Berlin", new Illustration("Incorrect answer", "image4.test"));
-        alternatives01.addAll(Set.of(alternative01a, alternative01b, alternative01c));
-
-        Set<Alternative> alternatives02 = new HashSet<>();
-        Alternative alternative02a = new Alternative('a', "Java", new Illustration("Correct answer", "image6.test"));
-        Alternative alternative02b = new Alternative('b', "Python", new Illustration("Correct answer", "image7.test"));
-        Alternative alternative02c = new Alternative('c', "HTML", new Illustration("Incorrect answer", "image8.test"));
-        alternatives02.addAll(Set.of(alternative02a, alternative02b, alternative02c));
-
-        Set<Alternative> alternatives03 = new HashSet<>();
-        Alternative alternative03a = new Alternative('a', "True", new Illustration("Incorrect answer", "image10.test"));
-        Alternative alternative03b = new Alternative('b', "False", new Illustration("Correct answer", "image11.test"));
-        alternatives03.addAll(Set.of(alternative03a, alternative03b));
-
-        Question question1 = new Question(
+        QuestionRequestDTO questionRequest1 = new QuestionRequestDTO(
             QuestionType.ABERTA,
             "What is the capital of France?",
-            new Illustration("A map of Paris", "image1.test"),
-            alternatives01, Difficulty.FACIL,"a"
+            new IllustrationRequestDTO("A map of Paris", "image1.test"),
+            Set.of(
+            new AlternativesRequestDTO('a', "Paris", new IllustrationRequestDTO("Correct answer", "image2.test")),
+            new AlternativesRequestDTO('b', "London", new IllustrationRequestDTO("Incorrect answer", "image3.test")),
+            new AlternativesRequestDTO('c', "Berlin", new IllustrationRequestDTO("Incorrect answer", "image4.test"))
+            ),
+            Set.of(),
+            Difficulty.FACIL,
+            "a"
         );
 
-        Question question2 = new Question(
+        QuestionRequestDTO questionRequest2 = new QuestionRequestDTO(
             QuestionType.FECHADA,
             "Which of the following are programming languages?",
-            new Illustration("Snippet of code", "image5.test"),
-            alternatives02, Difficulty.FACIL,"a,b"
+            new IllustrationRequestDTO("Snippet of code", "image5.test"),
+            Set.of(
+            new AlternativesRequestDTO('a', "Java", new IllustrationRequestDTO("Correct answer", "image6.test")),
+            new AlternativesRequestDTO('b', "Python", new IllustrationRequestDTO("Correct answer", "image7.test")),
+            new AlternativesRequestDTO('c', "HTML", new IllustrationRequestDTO("Incorrect answer", "image8.test"))
+            ),
+            Set.of(),
+            Difficulty.FACIL,
+            "a,b"
         );
 
-        Question question3 = new Question(
+        QuestionRequestDTO questionRequest3 = new QuestionRequestDTO(
             QuestionType.FECHADA,
             "The Earth is flat.",
-            new Illustration("Flat Earth illustration", "image9.test"),
-            alternatives03, Difficulty.FACIL,"b"
+            new IllustrationRequestDTO("Flat Earth illustration", "image9.test"),
+            Set.of(
+            new AlternativesRequestDTO('a', "True", new IllustrationRequestDTO("Incorrect answer", "image10.test")),
+            new AlternativesRequestDTO('b', "False", new IllustrationRequestDTO("Correct answer", "image11.test"))
+            ),
+            Set.of(),
+            Difficulty.FACIL,
+            "b"
         );
+
+        Question question1 = new Question(questionRequest1);
+        Question question2 = new Question(questionRequest2);
+        Question question3 = new Question(questionRequest3);
+
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
         questions.add(question1);
         questions.add(question2);
         questions.add(question3);
@@ -172,22 +188,40 @@ class QuestionServiceImplTest {
     @Test
     void givenValidQuestion_whenSave_thenReturnEntity() {
         Question questionExpected = questions.get(0);
+        Category category = new Category("Portugues", Origin.DISCIPLINA);
+        Set<String> categorias = Set.of(category.getName());
         when(questionRepository.save(questionExpected)).thenReturn(questionExpected);
-        Question questionResult = questionService.save(questionExpected);
+        when(categoryService.findByNameIn(categorias)).thenReturn(Set.of(category));
+
+        Question questionResult = questionService.save(questionExpected, categorias);
         assertEquals(questionExpected, questionResult);
+        assertFalse(questionResult.getCategories().isEmpty());
         verify(questionRepository).save(questionExpected);
+        verify(categoryService).findByNameIn(categorias);
     }
 
     @Test
     void givenExistingId_whenUpdate_thenUpdateEntity() {
         Question question = questions.get(0);
         question.setId(UUID.randomUUID());
-        QuestionRequestDTO dto = new QuestionRequestDTO(QuestionType.FECHADA,
+        QuestionRequestDTO dto = new QuestionRequestDTO(
+            QuestionType.FECHADA,
             "Updated question title",
-            new Illustration("Updated illustration description", "updated_image.test"),  
-            Set.of(new Alternative('a', "Updated alternative", new Illustration("Updated alt description", "alt_image.test"))),
-            Difficulty.MEDIO,
-            "a");
+            new IllustrationRequestDTO(
+            "Updated illustration description",
+            "updated_image.test"
+            ),
+            Set.of(
+            new AlternativesRequestDTO(
+                'b',
+                "Updated alternative text",
+                new IllustrationRequestDTO("Updated alternative illustration description", "updated_alt_image.test")
+            )
+            ),
+            Set.of("Portugues"),
+            Difficulty.DIFICIL,
+            "b"
+        );
         Question questioUpdate = new Question(dto);
         when(questionRepository.existsById(question.getId())).thenReturn(true);
         when(questionRepository.save(questioUpdate)).thenReturn(questioUpdate);
