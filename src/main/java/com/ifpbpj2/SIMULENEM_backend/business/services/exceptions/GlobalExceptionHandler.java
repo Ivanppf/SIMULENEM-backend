d4fee899f26ef8486e073aba3e43c687c10e0231
robsoncaliban <robsoncaliban@gmail.com>
2025-05-26 00:46:32 -0300

    fix: corrigido o tratamento de exceções para usar HttpServletRequest em vez de WebRequest

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/business/services/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/business/services/exceptions/GlobalExceptionHandler.java
@@ -10,15 +10,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, HttpServletRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(
                 LocalDateTime.now(),
                 ex.getMessage(),
-                request.getDescription(false));
+                request.getRequestURI());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
