40c7548b2bca368fd8e2042c08ef1a0c7abaedd1
robsoncaliban <robsoncaliban@gmail.com>
2025-05-26 00:47:26 -0300

    feat: adicionada definição OpenAPI para a API SimulENEM com informações de título, versão e descrição

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplication.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplication.java
@@ -3,7 +3,11 @@ package com.ifpbpj2.SIMULENEM_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SimulENEM", version = "1", description = "API desenvolvida para elabora provas"))
 public class SimulenemBackendApplication {
 
 	public static void main(String[] args) {
