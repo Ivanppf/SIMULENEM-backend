91164f0866f4415311265db214ed50634ba6ae71
robsoncaliban <robsoncaliban@gmail.com>
2025-05-26 22:41:18 -0300

    refactor: renomeado campo 'questions' para 'questionExams' em SectionRequestDTO e adicionado campo 'title' em ExamRequestDTO

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/request/ExamRequestDTO.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/request/ExamRequestDTO.java
@@ -3,6 +3,7 @@ package com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request;
 import java.time.LocalDate;
 
 public record ExamRequestDTO(
+    String title,
     LocalDate applicationDate,
     Boolean resultPublished
 ) {
--- src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/request/SectionRequestDTO.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/request/SectionRequestDTO.java
@@ -9,7 +9,7 @@ public record SectionRequestDTO(
     @NotNull
     Integer position,
     String title,
-    Set<QuestionExamRequestDTO> questions
+    Set<QuestionExamRequestDTO> questionExams
 ) {
 
 }
