80c227fdcd8d5ee8158cef0bdb2a1f34a4f00df4
Ivan Pinheiro <Ivanppf1@gmail.com>
2025-05-31 10:32:03 -0300

    fix: correção em descrição

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplication.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/SimulenemBackendApplication.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "SimulENEM", version = "1", description = "API desenvolvida para elabora provas"))
+@OpenAPIDefinition(info = @Info(title = "SimulENEM", version = "1", description = "API desenvolvida para elaborar provas"))
 public class SimulenemBackendApplication {
 
 	public static void main(String[] args) {
