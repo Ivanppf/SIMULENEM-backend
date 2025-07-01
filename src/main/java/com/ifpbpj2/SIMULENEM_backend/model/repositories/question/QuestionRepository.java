86b9684e6173077eed3c9f476dfe9711e29bbbfb
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 20:28:37 -0300

    feat: adiciona método findAllPageable ao QuestionRepository para suporte a paginação

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/model/repositories/question/QuestionRepository.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/model/repositories/question/QuestionRepository.java
@@ -2,8 +2,16 @@ package com.ifpbpj2.SIMULENEM_backend.model.repositories.question;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ifpbpj2.SIMULENEM_backend.model.entities.question.Question;
+import com.ifpbpj2.SIMULENEM_backend.model.repositories.question.projections.QuestionProjection;
 
-public interface QuestionRepository extends JpaRepository<Question, UUID>{}
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
+        @Query("SELECT q FROM Question q")
+        Page<QuestionProjection> findAllPageable(Pageable pageable);
+
+}
