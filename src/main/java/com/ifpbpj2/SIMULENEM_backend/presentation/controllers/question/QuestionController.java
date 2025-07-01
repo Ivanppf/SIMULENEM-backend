821029a12a962468651c5ef267db1947f8310db1
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-01 20:30:51 -0300

    refactor: simplifica método findAll no QuestionController para retornar projeções de questões pageadas

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/controllers/question/QuestionController.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/controllers/question/QuestionController.java
@@ -1,34 +1,21 @@
 package com.ifpbpj2.SIMULENEM_backend.presentation.controllers.question;
 
-import com.ifpbpj2.SIMULENEM_backend.model.entities.question.Question;
-import com.ifpbpj2.SIMULENEM_backend.model.enums.Difficulty;
-import com.ifpbpj2.SIMULENEM_backend.model.enums.QuestionType;
+import com.ifpbpj2.SIMULENEM_backend.model.repositories.question.projections.QuestionProjection;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.PageableDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.QuestionRequestDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response.QuestionResponseDTO;
-import jakarta.validation.Valid;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
 public interface QuestionController {
 
-    ResponseEntity<Page<QuestionResponseDTO>> find(
-            Pageable pageable,
-            UUID id,
-            QuestionType questionType,
-            String title,
-            Set<String> categories,
-            Difficulty difficulty,
-            LocalDateTime lastUsedDate);
+    ResponseEntity<PageableDTO<QuestionProjection>> findAll(Pageable pageable);
 
-    ResponseEntity<QuestionResponseDTO> findById( UUID id);
+    ResponseEntity<QuestionResponseDTO> findById(UUID id);
 
     ResponseEntity<QuestionResponseDTO> save(QuestionRequestDTO obj);
 
