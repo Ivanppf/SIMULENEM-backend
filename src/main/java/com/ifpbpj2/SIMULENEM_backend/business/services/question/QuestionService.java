cf6be4a020c9c932ec37143d349bba0795b6c21a
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-14 17:28:13 -0300

    refactor: atualiza QuestionService para retornar Pageable de QuestionResponseDTO e suportar query parameters

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/business/services/question/QuestionService.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/business/services/question/QuestionService.java
@@ -8,13 +8,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.ifpbpj2.SIMULENEM_backend.model.entities.question.Question;
-import com.ifpbpj2.SIMULENEM_backend.model.repositories.question.projections.QuestionProjection;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.queryParameters.QuestionQueryParametersDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.QuestionRequestDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response.CategoryResponseDTO;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response.QuestionResponseDTO;
 
 public interface QuestionService {
 
-    Page<QuestionProjection> findAll(Pageable pageable);
+    Page<QuestionResponseDTO> findAll(Pageable pageable, QuestionQueryParametersDTO queryParametersDTO);
 
     Question findById(UUID uuid);
 
