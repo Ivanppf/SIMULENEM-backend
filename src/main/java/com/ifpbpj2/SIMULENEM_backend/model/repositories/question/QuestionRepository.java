e99a02d6068c11ef93e45af3f3ca5b2c521a821a
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-14 17:30:44 -0300

    feat: adiciona interface JpaSpecificationExecutor para especificação de filtro de questões ao QuestionRepository

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/model/repositories/question/QuestionRepository.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/model/repositories/question/QuestionRepository.java
@@ -2,16 +2,10 @@ package com.ifpbpj2.SIMULENEM_backend.model.repositories.question;
 
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ifpbpj2.SIMULENEM_backend.model.entities.question.Question;
-import com.ifpbpj2.SIMULENEM_backend.model.repositories.question.projections.QuestionProjection;
 
-public interface QuestionRepository extends JpaRepository<Question, UUID> {
-        @Query("SELECT q FROM Question q")
-        Page<QuestionProjection> findAllPageable(Pageable pageable);
-
+public interface QuestionRepository extends JpaRepository<Question, UUID>, JpaSpecificationExecutor<Question> {
 }
