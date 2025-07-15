2801667bd4e936740e3144b05c20321c04c0ec39
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-15 09:10:54 -0300

    feat: adiciona QueryParameters ao findAll de QuestionController

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/controllers/question/QuestionController.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/controllers/question/QuestionController.java
@@ -1,19 +1,22 @@
 package com.ifpbpj2.SIMULENEM_backend.presentation.controllers.question;
 
-import com.ifpbpj2.SIMULENEM_backend.model.repositories.question.projections.QuestionProjection;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.PageableDTO;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.queryParameters.QuestionQueryParametersDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.QuestionRequestDTO;
 import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response.QuestionResponseDTO;
 
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface QuestionController {
 
-    ResponseEntity<PageableDTO<QuestionProjection>> findAll(Pageable pageable);
+    ResponseEntity<PageableDTO<QuestionResponseDTO>> findAll(@ModelAttribute QuestionQueryParametersDTO queryParameters,
+            @ParameterObject Pageable pageable);
 
     ResponseEntity<QuestionResponseDTO> findById(UUID id);
 
