e584c2c2fc60eaacdff122c115a6d0046b4f4677
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-20 00:53:47 -0300

    refactor: remove implementação do hateoas do ExamControllerImpl e ExamServiceImpl

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/response/ExamResponseDTO.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/DTO/response/ExamResponseDTO.java
@@ -3,31 +3,17 @@ package com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response;
 import java.time.LocalDate;
 import java.util.UUID;
 
-import org.springframework.hateoas.Link;
-
 import com.ifpbpj2.SIMULENEM_backend.model.entities.exam.Exam;
 
-
 public record ExamResponseDTO(
-    UUID id,
-    String title,
-    LocalDate applicationDate,
-    boolean resultPublished,
-    Double totalScore,
-    Link sections
-) {
-    public ExamResponseDTO(Exam exam, Link secLink) {
-       this(exam.getId(), exam.getTitle(), exam.getApplicationDate(), 
-       exam.isResultPublished(), exam.getTotalScore(), secLink);
+        UUID id,
+        String title,
+        LocalDate applicationDate,
+        boolean resultPublished,
+        Double totalScore) {
+    public ExamResponseDTO(Exam exam) {
+        this(exam.getId(), exam.getTitle(), exam.getApplicationDate(),
+                exam.isResultPublished(), exam.getTotalScore());
     }
-    public ExamResponseDTO withSections(Link newSections) {
-        return new ExamResponseDTO(
-            this.id,
-            this.title,
-            this.applicationDate,
-            this.resultPublished,
-            this.totalScore,
-            newSections
-        );
-    }
+
 }
