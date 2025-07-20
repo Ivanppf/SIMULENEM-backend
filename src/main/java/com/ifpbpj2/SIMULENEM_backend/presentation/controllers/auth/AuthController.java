5da7431db148b5ed518e503461c4a94eb71852c5
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-20 01:08:16 -0300

    feat: adiciona AuthController com método de login

---
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/presentation/controllers/auth/AuthController.java
@@ -0,0 +1,44 @@
+package com.ifpbpj2.SIMULENEM_backend.presentation.controllers.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpbpj2.SIMULENEM_backend.jwt.UserDetailsJwtService;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.request.LoginDTO;
+import com.ifpbpj2.SIMULENEM_backend.presentation.DTO.response.TokenDTO;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+        private final UserDetailsJwtService userDetailsJwtService;
+        private final AuthenticationManager authenticationManager;
+
+        public AuthController(UserDetailsJwtService userDetailsJwtService,
+                        AuthenticationManager authenticationManager) {
+                this.userDetailsJwtService = userDetailsJwtService;
+                this.authenticationManager = authenticationManager;
+        }
+
+        @PostMapping("/login")
+        public ResponseEntity<TokenDTO> login(@Valid @RequestBody LoginDTO loginDto) {
+
+                var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                                loginDto.username(),
+                                loginDto.password());
+
+                authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+                var token = userDetailsJwtService.getTokenAuthenticated(loginDto.username());
+
+                return ResponseEntity.ok(token);
+        }
+
+}
