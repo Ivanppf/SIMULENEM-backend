a5645cbca9272b663d5668754c986724b8ab1e65
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-22 11:44:57 -0300

    fix: corrige configuração do CORS no SecurityConfig

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/config/SecurityConfig.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ifpbpj2.SIMULENEM_backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.ifpbpj2.SIMULENEM_backend.infra.jwt.AuthorizationFilter;
@@ -33,6 +37,7 @@ public class SecurityConfig {
         @Bean
         SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
                 return httpSecurity
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                                 .csrf(csrf -> csrf.disable())
                                 .formLogin(form -> form.disable())
                                 .httpBasic(basic -> basic.disable())
@@ -61,6 +66,19 @@ public class SecurityConfig {
         @Bean
         AuthorizationFilter jwtAuthorizationFilter() {
                 return new AuthorizationFilter();
+        }
+
+        @Bean
+        UrlBasedCorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(
+                                Arrays.asList("http://localhost:3000", "https://your-frontend-domain.com"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("*"));
+                configuration.setAllowCredentials(true);
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
         }
 
 }
