a24e710b69202b46f135395c1bbc6bf166580b5f
Atos Alves <atosalvesqueiroz@gmail.com>
2025-07-29 14:22:25 -0300

    refactor: adiciona valores e métodos aos enums Difficulty, Origin e QuestionType

--- src/main/java/com/ifpbpj2/SIMULENEM_backend/model/enums/Origin.java
+++ src/main/java/com/ifpbpj2/SIMULENEM_backend/model/enums/Origin.java
@@ -1,7 +1,27 @@
 package com.ifpbpj2.SIMULENEM_backend.model.enums;
 
 public enum Origin {
-    AREA_CONHECIMENTO_ENEM,
-    DISCIPLINA,
-    ASSUNTO;
+    AREA_CONHECIMENTO_ENEM(1),
+    DISCIPLINA(2),
+    ASSUNTO(3);
+
+    private final int value;
+
+    private Origin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Origin fromInt(int value) {
+        for (Origin type : Origin.values()) {
+            if (type.value == value) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Valor inválido para QuestionType: " + value);
+    }
+
 }
